ef917ba1b4f65b5410de33de208db176950e7b41
free4mOriginal <free4m@gmail.com>
2019-09-29 22:38:44 -0500

    service account

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
-const functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require("firebase-functions");
+// const config = require("/util/config");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://newster-ac2aa.firebaseio.com"
+});
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push(doc.data());
+      });
+      return res.json(posts);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status("500").json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
