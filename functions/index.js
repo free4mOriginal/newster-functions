a82492ded5f6a2b9471c10f39096ded6e70014db
free4mOriginal <free4m@gmail.com>
2019-10-02 15:13:40 -0500

    fix token naming ambiguity

--- functions/index.js
+++ functions/index.js
@@ -84,6 +84,7 @@ app.post("/signup", (req, res) => {
   };
 
   // TODO - validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then(doc => {
@@ -96,9 +97,20 @@ app.post("/signup", (req, res) => {
       }
     })
     .then(data => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
