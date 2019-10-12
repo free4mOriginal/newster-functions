0aea8564b867fabae99b0578d23e188aefe8176e
free4mOriginal <free4m@gmail.com>
2019-10-11 21:36:47 -0500

    add dbschema info

--- functions/util/dbschema.js
+++ functions/util/dbschema.js
@@ -1,4 +1,4 @@
-let db = {
+const db = {
     posts: [
       {
         userHandle: 'user',
@@ -21,3 +21,26 @@ let db = {
       }
     ]
   };
+
+const userDetails = {
+  credentials: {
+    userId: 'XXRrdPf9jYSbV8Pbz4CYLfEZBDM2',
+    email: 'dummy6@email.com',
+    handle: 'dummy6',
+    createdAt: '2019-10-11T04:24:02.553Z',
+    imageUrl:  'https://firebasestorage.googleapis.com/v0/b/newster-ac2aa.appspot.com/o/519158690.jpg?alt=media',
+    bio: 'Hello, I am dummy6',
+    website: 'http://dummy6.com',
+    location: 'St. Louis, MO'
+  },
+  likes: [
+    {
+      userHandle: 'dummy1',
+      postId: 'zXQ5eUBvhKPtjVf3Nrt6'
+    },
+    {
+      userHandle: 'dummy5',
+      postId: 'zXQ5eUBvhKPtjVf3Nrt6'
+    }
+  ]
+}
